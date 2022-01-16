ccd5de182f7531fd87c7265a422ae4c8a0e45bf9
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-16 14:34:20 +0900

    useEffectを利用したタイマー

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Basic2 from './components/Basic2';
+import TimerContainer from './components/TimerContainer';
 
 function App() {
   return (
     <div className="App">
-      <Basic2 />
+      <TimerContainer />
     </div>
   );
 }
